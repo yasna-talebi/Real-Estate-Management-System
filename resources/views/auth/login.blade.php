b990613ad744374ec8dd39b81026d9d04dadc5f4
Alireza Talebi <alirezatalebi@home.local>
2025-01-18 15:55:03 +0100

    updated gitignore and added new button to login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,6 +42,11 @@
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
+            <a href="{{ route('register') }}">
+                <x-primary-button type='button' class="ms-4">
+                    {{ __('Register') }}
+                </x-primary-button>
+            </a>
         </div>
     </form>
 </x-guest-layout>
